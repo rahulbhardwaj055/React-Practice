07c8b788cd9ceda6bca67f6d1ca7f4c226af0614
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-01-25 12:58:33 +0530

    my third commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/person';
 
 function App() {
   return (
     <div className="App">
-      //  <h2>HI this is testing2</h2>
+        <h2>HI this is testing2</h2>
 
         <h1>HI this is testing</h1>
+        <Person/>
     </div>
   );
 }
