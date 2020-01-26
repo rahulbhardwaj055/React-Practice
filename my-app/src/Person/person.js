27e1e0eb687707f1faa953a904a0cf26caa3977d
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-01-26 16:51:19 +0530

    my forth commit

--- my-app/src/Person/person.js
+++ my-app/src/Person/person.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 
 const person = () => {
-    return <p>Hi I am a very nice person</p>
+    return <p>Hi I am a very nice person{
+        Math.floor(Math.random() * 30)
+    }</p>
 }
 
 export default person;
