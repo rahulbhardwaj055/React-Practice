fd731db16543a8c1e6bed7cdf991f45c5a61bc26
rahubhardwaj <rahubhardwaj@expediagroup.com>
2020-02-01 08:54:11 +0530

    working on state & prop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,19 +12,16 @@ function App() {
         ]
     }
 
-
-
     render(){
         return (
             <div className="App">
                 <h2>HI this is testing2</h2>
                 <h1>HI this is testing</h1>
-                <Person name={this.state.persons[0].name} age={this.state.age[0].name}/>
-                <Person name={this.state.persons[1].name} age={this.state.age[1].name}> Working in TCS </Person>
-                <Person name={this.state.persons[2].name} age={this.state.age[2].name}/>
+                <Person name={ this.state.persons[0].name} age={this.state.age[0].name} />
+                <Person name={this.state.persons[1].name} age={this.state.age[1].name} > Working in TCS </Person>
+                <Person name={this.state.persons[2].name} age={this.state.age[2].name} />
             </div>
         );
-
     }
 }
 
